f5b94f96c81ee5c1e731158b90a116fcca23462f
scasadykahler <scasadykahler3584@student.pps.net>
2024-10-25 13:53:01 -0700

    soiguhv

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -9,8 +9,8 @@ public class Spawner : MonoBehaviour
     void Start()
     {
         GameObject Player = PhotonNetwork.Instantiate("Player", new Vector3(Random.Range(-6, 0), 1, 2), Quaternion.identity);
-        GameObject Camera = PhotonNetwork.Instantiate("Camera", Player.transform.position, Quaternion.identity);
-        Camera.transform.parent = Player.transform;
+        //GameObject Camera = PhotonNetwork.Instantiate("Camera", Player.transform.position, Quaternion.identity);
+        //Camera.transform.parent = Player.transform;
         Camera.SetActive(true);
         //Camera.GetComponent<CameraScript>().player = Player;
         
