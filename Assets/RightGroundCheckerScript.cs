3f80ac7e5ecddc07af0075cfbffbf7950f92563c
scasadykahler <scasadykahler3584@student.pps.net>
2024-10-25 13:42:15 -0700

    hopes

--- Assets/RightGroundCheckerScript.cs
+++ Assets/RightGroundCheckerScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RightGroundCheckerScript : RightGroundCheckerScript
+public class RightGroundCheckerScript : GroundCheckerScript
 {
     public void OnTriggerEnter(Collider collider)
     {
