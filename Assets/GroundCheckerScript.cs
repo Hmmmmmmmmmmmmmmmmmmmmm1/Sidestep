bea3836fd8fbf738367cb8a39bf9145c300a2b7e
Casey <caseyluttkus@gmail.com>
2024-10-29 13:17:02 -0700

    hgjfdsakjfghkdsoghksogfdjlhkldgjhkjkghojgkdijlskhjjshdjajshksjkjfkjihhsjhfjijjhifdahsbkjsfkbfjsdbkjjbfjskahhfskadjbfhhhahjhfdgajhdsughfdsihkjdshafkdbsjhfkdhjsagfs

--- Assets/GroundCheckerScript.cs
+++ Assets/GroundCheckerScript.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class GroundCheckerScript : MonoBehaviour
 {
     public bool lGrounded;
     public bool rGrounded;
     
+    void Update()
+    {
+        if(!GetComponent<PhotonView>().IsMine)
+        {
+            Destroy(this.gameObject);
+        }
+    }
 }
