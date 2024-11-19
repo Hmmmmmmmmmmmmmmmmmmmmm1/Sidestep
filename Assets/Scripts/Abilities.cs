2be8a6aba50c860d2e7ed8ce7ee6f95de4391905
Rnguyen2019 <98835647+Rnguyen2019@users.noreply.github.com>
2024-11-19 13:51:30 -0800

    Working On Making My Ability Selector

--- Assets/Scripts/Abilities.cs
+++ Assets/Scripts/Abilities.cs
@@ -12,7 +12,7 @@ public class Abilities : MonoBehaviour
     //direction change
     //blink
     //launch                :)
-    //temp sturctures
+    //temp sturctures       :)
     //healing
 
     //Battle
@@ -23,11 +23,11 @@ public class Abilities : MonoBehaviour
     //knockback
 
     public enum MovementAbility {DirectionChange, Blink, Launch, LegoBuildMode, Heal}
-    public MovementAbility Skill1;
+    public String Skill1;
     public KeyCode Skill1Trigger;
 
     public enum BattleAbility {Lifesteal, Immobilize, FireAspectII, Knockback, doxx}
-    public BattleAbility Skill2;
+    public String Skill2;
     public KeyCode Skill2Trigger;
 
     private bool activated;
@@ -41,12 +41,12 @@ public class Abilities : MonoBehaviour
 
     //Lego
     private int legoesPerAbility = 0;
-    public GameObject legos;
+    private GameObject legos;
 
     // Start is called before the first frame update
     void Start()
     {
-    
+        legos = Resources.Load<GameObject>("Wall");
     }
 
     // Update is called once per frame
@@ -55,7 +55,7 @@ public class Abilities : MonoBehaviour
         Cooldown();
 
         //Lunch Mode
-        if (Skill1 == MovementAbility.Launch){
+        if (Skill1 == "Lynch"){
             if (Input.GetKeyDown(Skill1Trigger)){
 
             int lookingDown;
@@ -74,7 +74,7 @@ public class Abilities : MonoBehaviour
             }
 
             }
-            if(gameObject.GetComponent<Rigidbody>().velocity.y >= 20 && Skill1 == MovementAbility.Launch){
+            if(gameObject.GetComponent<Rigidbody>().velocity.y >= 20){
                 gameObject.GetComponent<Rigidbody>().velocity += new Vector3(0,-3,0);
             }
 
@@ -84,7 +84,7 @@ public class Abilities : MonoBehaviour
                 activated = false;
             }
         }
-        if (Skill1 == MovementAbility.LegoBuildMode){
+        if (Skill1 == "Duplo mode"){
             if (Input.GetKeyDown(Skill1Trigger) && Skill1Cooldown < 0){
                 Instantiate(legos, transform.position + transform.forward * 10 + Vector3.up * 2, legos.transform.rotation);
                 legoesPerAbility++;
@@ -103,5 +103,10 @@ public class Abilities : MonoBehaviour
 
         Skill1Text.text = Skill1Cooldown.ToString().Substring(0,1);
         Skill2Text.text = Skill2Cooldown.ToString().Substring(0,1);
+    }
+
+    public void getAbilities(String Skill1, String Skill2){
+        this.Skill1 = Skill1;
+        this.Skill2 = Skill2;
     }
 }
---
+++ Assets/Scripts/AbilitySelection.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AbilitySelection : MonoBehaviour
+{
+    private String Skill1;
+    private String Skill2;
+    public Text greg;
+    public Text rodrick;
+    public String frank;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void geagling(int fortnite){
+        if (fortnite ==0){
+            Skill1 = "Undecdided";
+        }
+        if (fortnite ==1){
+            Skill1 = "Lynch";
+        }
+        if (fortnite ==2){
+            Skill1 = "Duplo mode";
+        }
+        if (fortnite ==3){
+            Skill1 = "stop";
+        }
+        if (fortnite ==4){
+            Skill1 = "turn right";
+        }
+        if (fortnite ==5){
+            Skill1 = "used-bandaid";
+        }
+        greg.text = Skill1;
+    }
+
+    public void geagled(int fortnite){
+        if (fortnite ==0){
+            Skill2 = "Independent";
+        }
+        if (fortnite ==1){
+            Skill2 = "republiCANT";
+        }
+        if (fortnite ==2){
+            Skill2 = "DUMBocrat";
+        }
+        rodrick.text = Skill2;
+    }
+
+    public void sendScore(){
+        SceneManager.LoadScene(3);
+        GameObject.FindGameObjectsWithTag("Player")[0].GetComponent<Abilities>().getAbilities(Skill1,Skill2);
+    }
+}
