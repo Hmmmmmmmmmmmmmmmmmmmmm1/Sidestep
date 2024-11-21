31b70ae63ea9123d9d83e0dfcf29c167f57361f8
scasadykahler <scasadykahler3584@student.pps.net>
2024-11-21 14:01:42 -0800

    did some work on the quick join/create

--- Assets/Scripts/RoomList.cs
+++ Assets/Scripts/RoomList.cs
@@ -6,11 +6,14 @@ using Photon.Realtime;
 
 public class roomlist : MonoBehaviourPunCallbacks
 {
+    public GameObject RoomPrefab;
     public override void OnRoomListUpdate(List<RoomInfo> roomlist)
     {
         for (int i = 0; i < roomlist.Count; i++)
         {
             print(roomlist[i].Name);
+            GameObject Room = Instantiate(RoomPrefab, Vector3.zero, Quaternion.identity, GameObject.Find("Content").transform);
+            Room.GetComponent<Room>().Name.text = roomlist[i].Name;
         }
     }
 }
