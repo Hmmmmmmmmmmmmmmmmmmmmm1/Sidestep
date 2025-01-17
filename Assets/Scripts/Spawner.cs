b9c1531456b622da21e2e485bf918e4e7f19869f
Rnguyen2019 <98835647+Rnguyen2019@users.noreply.github.com>
2025-01-17 13:45:30 -0800

    garpple hok

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using ExitGames.Client.Photon.StructWrapping;
+using Unity.VisualScripting;
 
 public class Spawner : MonoBehaviour
 {
     public GameObject Camera;
+
     void Start()
     {
         GameObject Player = PhotonNetwork.Instantiate("Ian 1", new Vector3(Random.Range(-6, 0), 10, 2), Quaternion.identity);
