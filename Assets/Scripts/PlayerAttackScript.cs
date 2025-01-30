f7a22d71533d6b9a82ebcc541378263e78598c3d
Casey <caseyluttkus@gmail.com>
2025-01-30 13:43:37 -0800

    dsfgh

--- Assets/Scripts/PlayerAttackScript.cs
+++ Assets/Scripts/PlayerAttackScript.cs
@@ -57,7 +57,7 @@ namespace Assets.Scripts.CharacterControl
         public void Swing()
         {   
             time += Time.deltaTime;
-            SwordHolder.localPosition = Vector3.Lerp (oriental, pos, time/0.4);
+            SwordHolder.localPosition = Vector3.Lerp (oriental, pos, time/0.4f);
             Debug.Log(pos);
         }
 //        public float side = 1f;
