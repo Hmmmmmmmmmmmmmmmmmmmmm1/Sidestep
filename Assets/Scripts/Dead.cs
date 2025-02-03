b1f847c66b7a61c45adeb30137636ab70d3561c8
Rnguyen2019 <98835647+Rnguyen2019@users.noreply.github.com>
2025-02-03 12:27:48 -0800

    casey scene

--- Assets/Scripts/Dead.cs
+++ Assets/Scripts/Dead.cs
@@ -19,7 +19,7 @@ public class Dead : MonoBehaviour
     void Update()
     {
         Scene scene  = SceneManager.GetActiveScene();
-        if (!scene.name.Equals("Loser's Scene")){
+        if (!scene.name.Equals("Casey's Scene")){
             if (PlayerHP2.hp == 0){
                 SceneManager.LoadScene(4);
                 Cursor.lockState = CursorLockMode.None;
