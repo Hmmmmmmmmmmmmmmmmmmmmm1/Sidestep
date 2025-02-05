71a28ef96acbb8b98496acb07274906cb4b8d400
Rnguyen2019 <98835647+Rnguyen2019@users.noreply.github.com>
2025-02-05 13:48:37 -0800

    :(

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -19,7 +19,7 @@ public class Spawner : MonoBehaviour
     {
         Player = PhotonNetwork.Instantiate("Ian 1", new Vector3(Random.Range(-6, 0), 10, 2), Quaternion.identity);
         PV = Player.GetComponent<PhotonView>();
-        PV.RPC("RPC_Beam", RpcTarget.All);
+        PV.RPC("PlayerSpawn", RpcTarget.All);
         playerCount++;
         GameObject Camera = PhotonNetwork.Instantiate("Camera", new Vector3(Player.transform.position.x,Player.transform.position.y + 0.5f,Player.transform.position.z ), Quaternion.identity);
         Camera.transform.parent = Player.transform;
