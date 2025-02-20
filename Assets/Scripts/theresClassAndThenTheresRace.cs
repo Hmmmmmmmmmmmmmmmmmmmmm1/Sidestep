befcd9f90ff45b5f1de2e66a575f57fe9b5142d4
Rnguyen2019 <98835647+Rnguyen2019@users.noreply.github.com>
2025-02-20 13:13:25 -0800

    nvm ignore

--- Assets/Scripts/theresClassAndThenTheresRace.cs
+++ Assets/Scripts/theresClassAndThenTheresRace.cs
@@ -23,13 +23,10 @@ public class theresClassAndThenTheresRace : MonoBehaviour
     [PunRPC]
     void PlayerSpawn()
     {
-        gameObject.name = "Player " + seeingPeopleOnlyBasedOnTheirColor.playerCount;
-        //gameObject.transform.Find("Marker").GetComponent<MeshRenderer>().material = PlayerManager.GetComponent<seeingPeopleOnlyBasedOnTheirColor>().glows[seeingPeopleOnlyBasedOnTheirColor.playerCount - 1];
-
-        if (1 == 1){
-            gameObject.transform.Find("Marker").GetComponent<MeshRenderer>().material = PlayerManager.GetComponent<seeingPeopleOnlyBasedOnTheirColor>().glows[seeingPeopleOnlyBasedOnTheirColor.playerCount - 1];
-            PlayerManager.GetComponent<seeingPeopleOnlyBasedOnTheirColor>().newPlayerJoin();
-            Debug.Log(gameObject.transform.Find("Marker").GetComponent<MeshRenderer>().material.name);
-        }
+        GameObject grumb = GameObject.Find("Ian 1");
+        grumb.name = "Player " + seeingPeopleOnlyBasedOnTheirColor.playerCount;
+        grumb.transform.Find("Marker").GetComponent<MeshRenderer>().material = PlayerManager.GetComponent<seeingPeopleOnlyBasedOnTheirColor>().glows[seeingPeopleOnlyBasedOnTheirColor.playerCount - 1];
+        PlayerManager.GetComponent<seeingPeopleOnlyBasedOnTheirColor>().newPlayerJoin();
+        //Debug.Log(gameObject.transform.Find("Marker").GetComponent<MeshRenderer>().material.name);
     }
 }
