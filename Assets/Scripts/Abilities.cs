a2740c74f88e84acfcfac81e70fe718e0c951fd2
Rnguyen2019 <98835647+Rnguyen2019@users.noreply.github.com>
2025-05-07 13:28:40 -0700

    blind

--- Assets/Scripts/Abilities.cs
+++ Assets/Scripts/Abilities.cs
@@ -422,7 +422,7 @@ public class Abilities : MonoBehaviour
                 if (lensDistortion) lensDistortion.intensity.value = 0;
             }
         else{
-            if (lensDistortion) lensDistortion.intensity.value = -10 * (10 - blindTimer) + 100;
+            if (lensDistortion) lensDistortion.intensity.value = -5 * (10 - blindTimer) + 100;
         }
 
         blindTimer -= Time.deltaTime;
