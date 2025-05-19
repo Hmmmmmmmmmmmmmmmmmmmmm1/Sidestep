b626f2d13f36df612e7cd99539dcebe92758ac32
Rnguyen2019 <98835647+Rnguyen2019@users.noreply.github.com>
2025-05-19 12:34:47 -0700

    Colors are cool

--- Assets/Scripts/theresClassAndThenTheresRace.cs
+++ Assets/Scripts/theresClassAndThenTheresRace.cs
@@ -24,7 +24,9 @@ public class theresClassAndThenTheresRace : MonoBehaviour
         GameObject target = GameObject.Find("Ian 1(Clone)");
         if (PlayerManager && target){
             target.name = "Player " + seeingPeopleOnlyBasedOnTheirColor.playerCount;
+            target.GetComponent<LineRenderer>().material = PlayerManager.GetComponent<seeingPeopleOnlyBasedOnTheirColor>().glows[seeingPeopleOnlyBasedOnTheirColor.playerCount - 1];
             target.transform.Find("Marker").GetComponent<MeshRenderer>().material = PlayerManager.GetComponent<seeingPeopleOnlyBasedOnTheirColor>().glows[seeingPeopleOnlyBasedOnTheirColor.playerCount - 1];
+            target.transform.Find("Sword Holder/Sword/Trail").GetComponent<ParticleSystemRenderer>().trailMaterial = PlayerManager.GetComponent<seeingPeopleOnlyBasedOnTheirColor>().glows[seeingPeopleOnlyBasedOnTheirColor.playerCount - 1];
             PlayerManager.GetComponent<seeingPeopleOnlyBasedOnTheirColor>().newPlayerJoin();
         //Debug.Log(gameObject.transform.Find("Marker").GetComponent<MeshRenderer>().material.name);
         }
